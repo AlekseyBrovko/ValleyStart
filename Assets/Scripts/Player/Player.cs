febe4dd086d3a4758cf49e5f4437eef2d4284d6d
AlekseyBrovko <96891019+AlekseyBrovko@users.noreply.github.com>
2022-07-14 00:39:47 +0300

    ---

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class Player : SingletoneMonobehaviour<Player>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
