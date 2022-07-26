0fac9371eb05974d3ce169af2375d54843e356c0
AlekseyBrovko <96891019+AlekseyBrovko@users.noreply.github.com>
2022-07-27 00:50:37 +0300

    ошибки блять

--- Assets/Scripts/Events/EventHandler.cs
+++ Assets/Scripts/Events/EventHandler.cs
@@ -1,10 +1,10 @@
-﻿public delegate void MovementDelegate(float xInput, float yInput, bool isWalking, bool isRunning, bool isIdle, bool isCarrying,
+﻿public delegate void MovementDelegate(float inputX, float inputY, bool isWalking, bool isRunning, bool isIdle, bool isCarrying,
     ToolEffect toolEffect,
     bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingToolDown,
     bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown,
     bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown,
     bool isSwingingToolRight, bool isSwingingToolLeft, bool isSwingingToolUp, bool isSwingingToolDown,
-    bool isIdleUp, bool isIdleDown, bool isIdleLeft, bool isIdleRight);
+    bool idleUp, bool idleDown, bool idleLeft, bool idleRight);
 
 public static class EventHandler
 {
@@ -14,21 +14,21 @@ public static class EventHandler
 
     //Movement Event Call Publishers
 
-    public static void CallMovementEvent(float xInput, float yInput, bool isWalking, bool isRunning, bool isIdle, bool isCarrying,
+    public static void CallMovementEvent(float inputX, float inputY, bool isWalking, bool isRunning, bool isIdle, bool isCarrying,
     ToolEffect toolEffect,
     bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingToolDown,
     bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown,
     bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown,
     bool isSwingingToolRight, bool isSwingingToolLeft, bool isSwingingToolUp, bool isSwingingToolDown,
-    bool isIdleUp, bool isIdleDown, bool isIdleLeft, bool isIdleRight)
+    bool idleUp, bool idleDown, bool idleLeft, bool idleRight)
     {
         if (MovementEvent!=null)
-            MovementEvent(xInput, yInput, isWalking, isRunning, isIdle, isCarrying,
+            MovementEvent(inputX, inputY, isWalking, isRunning, isIdle, isCarrying,
                 toolEffect,
                 isUsingToolRight, isUsingToolLeft, isUsingToolUp, isUsingToolDown,
                 isLiftingToolRight, isLiftingToolLeft, isLiftingToolUp, isLiftingToolDown,
                 isPickingRight, isPickingLeft, isPickingUp, isPickingDown,
                 isSwingingToolRight, isSwingingToolLeft, isSwingingToolUp, isSwingingToolDown,
-                isIdleUp, isIdleDown, isIdleLeft, isIdleRight);
+                idleUp, idleDown, idleLeft, idleRight);
     }
 }
